c8c030969430c5e48783546afbe223c4db7b301d
Orlando Leite <orlleite@gmail.com>
2018-10-17 13:11:08 -0400

    Fixed indentation

--- index.js
+++ index.js
@@ -6,18 +6,18 @@
 
 Object.defineProperty(String.prototype, 'firstCharToLowerCase',
 {
-	enumerable: false,
-	value: function ()
-	{
-		return this[0].toLowerCase() + this.substring(1);
-	}
+    enumerable: false,
+    value: function ()
+    {
+        return this[0].toLowerCase() + this.substring(1);
+    }
 });
 
 Object.defineProperty(String.prototype, 'firstCharToUpperCase',
 {
-	enumerable: false,
-	value: function (str)
-	{
-		return this[0].toUpperCase() + this.substring(1);
-	}
+    enumerable: false,
+    value: function (str)
+    {
+        return this[0].toUpperCase() + this.substring(1);
+    }
 });
